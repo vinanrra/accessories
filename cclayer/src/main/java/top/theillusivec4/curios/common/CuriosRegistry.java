b4eeb29c07da8b5000c46062584ab677615e88df
Dragon-Seeker <timekeeperguild@gmail.com>
2024-05-16 23:48:23 -0500

    Add CuriosContainerV2 and such

--- cclayer/src/main/java/top/theillusivec4/curios/common/CuriosRegistry.java
+++ cclayer/src/main/java/top/theillusivec4/curios/common/CuriosRegistry.java
@@ -9,12 +9,16 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.common.inventory.container.CuriosContainer;
+import top.theillusivec4.curios.common.inventory.container.CuriosContainerV2;
+
+import java.util.function.Supplier;
 
 public class CuriosRegistry {
 
     private static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(Registries.MENU, CuriosApi.MODID);
 
     public static final RegistryObject<MenuType<CuriosContainer>> CURIO_MENU = MENU_TYPES.register("curios_container", () -> IForgeMenuType.create(CuriosContainer::new));
+    public static final RegistryObject<MenuType<CuriosContainerV2>> CURIO_MENU_NEW = MENU_TYPES.register("curios_container_v2", () -> IForgeMenuType.create(CuriosContainerV2::new));
 
     public static void init() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
